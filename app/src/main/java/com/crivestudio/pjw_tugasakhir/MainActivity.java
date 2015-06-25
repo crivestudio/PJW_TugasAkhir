b4ef5ec9860694d0248f87cab533a7e01c80b1cd
Thoriq Satriya <thoriq.satriya94@gmail.com>
2015-06-25 19:20:56 +0700

    ganti webservice pake google maps

--- app/src/main/java/com/crivestudio/pjw_tugasakhir/MainActivity.java
+++ app/src/main/java/com/crivestudio/pjw_tugasakhir/MainActivity.java
@@ -16,70 +16,20 @@ import org.apache.http.StatusLine;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.List;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity
+{
 
-    public String readJSONFeed(String URL) {
-        StringBuilder stringBuilder = new StringBuilder();
-        HttpClient httpClient = new DefaultHttpClient();
-        HttpGet httpGet = new HttpGet(URL);
-        try {
-            HttpResponse response = httpClient.execute(httpGet);
-            StatusLine statusLine = response.getStatusLine();
-            int statusCode = statusLine.getStatusCode();
-            if (statusCode == 200) {
-                HttpEntity entity = response.getEntity();
-                InputStream inputStream = entity.getContent();
-                BufferedReader reader = new BufferedReader(
-                        new InputStreamReader(inputStream));
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    stringBuilder.append(line);
-                }
-                inputStream.close();
-            } else {
-                Log.d("JSON", "Failed to download file");
-            }
-        } catch (Exception e) {
-            Log.d("readJSONFeed", e.getLocalizedMessage());
-        }
-        return stringBuilder.toString();
-    }
-
-    private class ReadWeatherJSONFeedTask extends AsyncTask<String, Void, String> {
-
-        //test commit pake ANDROID STUDIO LANGSUNG
-        protected String doInBackground(String... urls) {
-            return readJSONFeed(urls[0]);
-        }
-
-
-        protected void onPostExecute(String result) {
-            try {
-                JSONObject jsonObject = new JSONObject(result);
-                JSONObject weatherObservationItems =
-                        new JSONObject(jsonObject.getString("weatherObservation"));
-
-                Toast.makeText(getBaseContext(),
-                        weatherObservationItems.getString("temperature") +
-                                " - " + weatherObservationItems.getString("clouds") +
-                                " - " + weatherObservationItems.getString("stationName"),
-                        Toast.LENGTH_SHORT).show();
-
-            } catch (Exception e) {
-                Log.d("ReadWeatherJSONFeedTask", e.getLocalizedMessage());
-            }
-        }
-    }
-
-
-
+    String apiKey = "AIzaSyA5csgc61wsvMf5QBumMTzEZLZ9pavLPlA";
+        
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -113,8 +63,61 @@ public class MainActivity extends ActionBarActivity {
         String txtLong = "-122.419416";
 
         new ReadWeatherJSONFeedTask().execute(
-                "http://ws.geonames.org/findNearByWeatherJSON?lat=" +
-                        txtLat + "&lng=" +
-                        txtLong + "&username=thoriq");
+                "https://maps.googleapis.com/maps/api/geocode/json?address=kalasan&key="
+                        + apiKey);
+    }
+
+
+
+
+    public String readJSONFeed(String URL) {
+        StringBuilder stringBuilder = new StringBuilder();
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpGet httpGet = new HttpGet(URL);
+        try {
+            HttpResponse response = httpClient.execute(httpGet);
+            StatusLine statusLine = response.getStatusLine();
+            int statusCode = statusLine.getStatusCode();
+            if (statusCode == 200) {
+                HttpEntity entity = response.getEntity();
+                InputStream inputStream = entity.getContent();
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(inputStream));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line);
+                }
+                inputStream.close();
+            } else {
+                Log.d("JSON", "Failed to download file");
+            }
+        } catch (Exception e) {
+            Log.d("readJSONFeed", e.getLocalizedMessage());
+        }
+        return stringBuilder.toString();
+    }
+
+    private class ReadWeatherJSONFeedTask extends AsyncTask<String, Void, String> {
+
+        protected String doInBackground(String... urls) {
+            return readJSONFeed(urls[0]);
+        }
+
+
+        protected void onPostExecute(String result) {
+            try {
+                JSONObject jsonObj = new JSONObject(result);
+
+                JSONArray results = jsonObj.getJSONArray("results");
+                JSONObject r = results.getJSONObject(0);
+
+                Toast.makeText(getBaseContext(),
+                        r.getString("formatted_address"),
+                        Toast.LENGTH_SHORT).show();
+
+            } catch (Exception e) {
+                Log.d("ReadWeatherJSONFeedTask", e.getLocalizedMessage());
+            }
+        }
     }
 }

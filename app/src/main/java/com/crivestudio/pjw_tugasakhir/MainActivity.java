d01aff8cc52d5fa4e6ce8644610aeb1d3cf84ac4
Thoriq Satriya <thoriq.satriya94@gmail.com>
2015-06-25 13:34:30 +0700

    coba webservice

--- app/src/main/java/com/crivestudio/pjw_tugasakhir/MainActivity.java
+++ app/src/main/java/com/crivestudio/pjw_tugasakhir/MainActivity.java
@@ -1,12 +1,83 @@
 package com.crivestudio.pjw_tugasakhir;
 
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    public String readJSONFeed(String URL) {
+        StringBuilder stringBuilder = new StringBuilder();
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpGet httpGet = new HttpGet(URL);
+        try {
+            HttpResponse response = httpClient.execute(httpGet);
+            StatusLine statusLine = response.getStatusLine();
+            int statusCode = statusLine.getStatusCode();
+            if (statusCode == 200) {
+                HttpEntity entity = response.getEntity();
+                InputStream inputStream = entity.getContent();
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(inputStream));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line);
+                }
+                inputStream.close();
+            } else {
+                Log.d("JSON", "Failed to download file");
+            }
+        } catch (Exception e) {
+            Log.d("readJSONFeed", e.getLocalizedMessage());
+        }
+        return stringBuilder.toString();
+    }
+
+    private class ReadWeatherJSONFeedTask extends AsyncTask<String, Void, String> {
+
+        protected String doInBackground(String... urls) {
+            return readJSONFeed(urls[0]);
+        }
+
+
+        protected void onPostExecute(String result) {
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                JSONObject weatherObservationItems =
+                        new JSONObject(jsonObject.getString("weatherObservation"));
+
+                Toast.makeText(getBaseContext(),
+                        weatherObservationItems.getString("temperature") +
+                                " - " + weatherObservationItems.getString("clouds") +
+                                " - " + weatherObservationItems.getString("stationName"),
+                        Toast.LENGTH_SHORT).show();
+
+            } catch (Exception e) {
+                Log.d("ReadWeatherJSONFeedTask", e.getLocalizedMessage());
+            }
+        }
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +105,15 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void btnGetWeather(View view) {
+        String txtLat = "37.37749";
+        String txtLong = "-122.419416";
+
+        new ReadWeatherJSONFeedTask().execute(
+                "http://ws.geonames.org/findNearByWeatherJSON?lat=" +
+                        txtLat + "&lng=" +
+                        txtLong + "&username=thoriq");
     }
 }
